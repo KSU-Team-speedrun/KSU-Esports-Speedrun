5f20b0d24dc00cc30b13352d2d362310a09c0762
BuildTools <unconfigured@null.spigotmc.org>
2024-09-12 13:36:06 -0400

    Implemented ObjectiveManager and Speedrun into EntityDeath.java; Implemented getTarget() into killObjective.java; Added more setters & getters into Speedrun.java

--- src/main/java/edu/Kennesaw/ksumcspeedrun/Events/EntityDeath.java
+++ src/main/java/edu/Kennesaw/ksumcspeedrun/Events/EntityDeath.java
@@ -1,6 +1,10 @@
 package edu.Kennesaw.ksumcspeedrun.Events;
 
 import edu.Kennesaw.ksumcspeedrun.Main;
+import edu.Kennesaw.ksumcspeedrun.Objective.KillObjective;
+import edu.Kennesaw.ksumcspeedrun.Objective.Objective;
+import edu.Kennesaw.ksumcspeedrun.Objective.ObjectiveManager;
+import edu.Kennesaw.ksumcspeedrun.Speedrun;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
@@ -9,12 +13,28 @@ public class EntityDeath implements Listener {
 
     Main plugin;
 
+    private final ObjectiveManager objectives;
+    private final Speedrun speedrun;
+
     public EntityDeath(Main plugin) {
+
         this.plugin = plugin;
+        speedrun = plugin.getSpeedrun();
+        objectives = speedrun.getObjectives();
+
     }
 
     @EventHandler
     public void onEntityDeath(EntityDeathEvent e) {
-        // Living entity has been killed
+        if (speedrun.isStarted()) {
+            for (Objective o : objectives.getObjectives()) {
+                if (o.getType().equals(Objective.ObjectiveType.KILL)) {
+                    KillObjective ko = (KillObjective) o;
+                    if (ko.getTarget().equals(e.getEntityType())) {
+                        // target entity is killed
+                    }
+                }
+            }
+        }
     }
 }
